b5c9ef7b9a878573570734ca9f3e4e596fc28576
Vlasceanu Marius-Ionut <128095775+Vlasceanu-Marius-Ionut@users.noreply.github.com>
2023-07-20 00:49:36 +0300

    Learning

--- js/js.js
+++ js/js.js
@@ -4,4 +4,21 @@ function mainPage(){
     window.location.href = '';
   })
 }
-mainPage();
+
+//MORE LEARNING
+const ar = function(double) {
+  return double / 3
+}
+
+function nbrs (a,b,c,d) {
+  
+  const result2 = ar(a,b,c,d)
+  
+  const result = `The resut is ${result2}`
+  
+  return result
+}
+nbrs()
+const solution = nbrs(44 + 33 + 22 + 502);
+console.log(solution)
+
