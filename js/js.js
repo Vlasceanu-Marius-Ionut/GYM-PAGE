2d96f2e5f863b0d1f83164c1373c8c93064015b4
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-08-27 23:42:42 +0300

    upp

--- js/js.js
+++ js/js.js
@@ -9,3 +9,18 @@ const forImg = document.getElementById('forimg');
 forImg.addEventListener('click',() => {
     window.location.href = "https://vlasceanu-marius-ionut.github.io/GYM-PAGE/"
 })
+
+const forInsta = document.getElementById('Instagram');
+forInsta.addEventListener('click', () =>{
+    window.location.href
+})
+
+const forFacebook = document.getElementById('Facebook');
+forFacebook.addEventListener('click', () =>{
+    window.location.href
+})
+
+const telePhone = document.getElementById('Telefon');
+telePhone.addEventListener('click', () =>{
+    window.location.href
+})
