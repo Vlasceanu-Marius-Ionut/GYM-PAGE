b0e020240a0883cc2f73122fd9fdc8ea628078d1
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-09-04 23:01:06 +0300

    betterfunction

--- js/js.js
+++ js/js.js
@@ -1,27 +1,17 @@
 'use strict'
-//ACASA
-const mainPage = document.getElementById('mainpager');
-mainPage.addEventListener('click',() =>{
-    window.location.href = "https://vlasceanu-marius-ionut.github.io/GYM-PAGE/"
-})
-//ACASA
-const forImg = document.getElementById('forimg');
-forImg.addEventListener('click',() => {
-    window.location.href = "https://vlasceanu-marius-ionut.github.io/GYM-PAGE/"
-})
-//PRODUSE
-// const products = document.getElementById('produse');
-// products.addEventListener('click', ()=> {
-//     window.location.href="pg2.html"
-// })
-//INSTA
-const forInsta = document.getElementById('InstagramLink');
-forInsta.addEventListener('click', () =>{
-    window.location.href = 'https://www.instagram.com/classicgymcolentina/'
-})
-//FACEBOOK
-const forFacebook = document.getElementById('FacebookLink');
-forFacebook.addEventListener('click', () =>{
-    window.location.href = 'https://www.facebook.com/misternetfitnessclub'
-})
-//TELEFON
+
+//clickerFunction
+function clicker(id, url){
+    const clickering = document.getElementById(id)
+    if(clickering){
+        addEventListener('click', () =>{
+            window.location.href = url
+        })
+    }
+}
+clicker('mainpager', 'https://vlasceanu-marius-ionut.github.io/GYM-PAGE/');
+clicker('forimg', 'https://vlasceanu-marius-ionut.github.io/GYM-PAGE/');
+clicker('InstagramLink', 'https://www.instagram.com/classicgymcolentina/');
+clicker('FacebookLink', 'https://www.facebook.com/misternetfitnessclub');
+
+  
