ebd0184db944319135efd1a47f6ad1882e329a9c
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-09-04 20:50:59 +0300

    jsjs2

---
+++ js/js2.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const mainpager = document.getElementById('mainpager');
+mainpager.addEventListener('click', ()=> {
+    window.location.href = "https://vlasceanu-marius-ionut.github.io/GYM-PAGE/"
+})
+
+const products = document.getElementById('produse');
+products.addEventListener('click', ()=> {
+    window.location.href=""
+})
