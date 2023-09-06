bb453d2c5b56a435cd16303381cd4689863e566a
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-09-06 23:02:36 +0300

    more

--- js/js.js
+++ js/js.js
@@ -49,7 +49,6 @@ window.addEventListener('resize', () =>{
 //slider
 let i = 0;
 let images = [];
-let time = 2000;
 
 images[0] = './images/img1.jpg';
 images[1] = './images/img2.jpg';
@@ -57,14 +56,21 @@ images[2] = './images/img4.jpg';
 images[3] = './images/img5.jpg';
 
 function changeimg() {
-    document.getElementById('slide').src = images[i];
+  const slide = document.getElementById('slide');
   
-    if (i < images.length - 1) {
-      i++;
-    } else {
-      i = 0;
-    }
-    setTimeout(changeimg, time);
+  i++;
+  if (i >= images.length) {
+    i = 0;
   }
   
-  window.onload = changeimg;
+  slide.src = images[i];
+}
+
+window.onload = function() {
+  changeimg(); 
+  const slide = document.getElementById('slide');
+  
+  slide.addEventListener('click', function() {
+    changeimg();
+  });
+};
