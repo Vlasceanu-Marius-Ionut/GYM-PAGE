1ac8f16d56093242505697de55f32914d0ded1e3
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-09-06 17:57:12 +0300

    shiny

--- js/js.js
+++ js/js.js
@@ -15,14 +15,33 @@ products.addEventListener('click', ()=> {
     window.location.href="pg2.html"
 })
 
-const forInsta = document.getElementById('Instagram');
+const forInsta = document.getElementById('InstagramLink');
 forInsta.addEventListener('click', () =>{
     window.location.href="https://www.instagram.com/classicgymcolentina/"
 })
 
-const forFacebook = document.getElementById('Facebook');
+const forFacebook = document.getElementById('FacebookLink');
 forFacebook.addEventListener('click', () =>{
     window.location.href="https://www.facebook.com/misternetfitnessclub"
 })
 
-let burgermenu;
+//BURGERMENU
+
+const burgermenu = document.createElement('div');
+burgermenu.classList.add('burgermenus');
+
+const hamburger = document.createElement('div');
+hamburger.classList.add('burgers')
+
+document.body.appendChild(burgermenu)
+burgermenu.appendChild(hamburger)
+
+const mainNav = document.querySelector('nav');
+
+burgermenu.addEventListener('click', () =>{
+    mainNav.classList.toggle('show-menu');
+});
+
+window.addEventListener('resize', () =>{
+    mainNav.classList.remove('show-menu')
+});
