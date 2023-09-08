844f9586350bffb5b1dafe34a56d1903dc55e08d
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-09-08 21:16:02 +0300

    ++

---
+++ js/js5.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const mainPage = document.getElementById('mainpager');
+mainPage.addEventListener('click',() =>{
+    window.location.href = "https://vlasceanu-marius-ionut.github.io/GYM-PAGE/"
+})
+
+const forImg = document.getElementById('forimg');
+forImg.addEventListener('click',() => {
+    window.location.href = "https://vlasceanu-marius-ionut.github.io/GYM-PAGE/"
+})
+
+const products = document.getElementById('products');
+products.addEventListener('click', ()=> {
+    window.location.href="produse.html"
+})
+
+const forInsta = document.getElementById('Instagram');
+forInsta.addEventListener('click', () =>{
+    window.location.href="https://www.instagram.com/classicgymcolentina/"
+})
+
+const forFacebook = document.getElementById('Facebook');
+forFacebook.addEventListener('click', () =>{
+    window.location.href="https://www.facebook.com/misternetfitnessclub"
+})
