1040eca62691f39a4c8a758c3bbd82c595496445
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-09-11 23:08:00 +0300

    Clock

--- js/js4.js
+++ js/js4.js
@@ -39,3 +39,28 @@ const forFacebook = document.getElementById('FacebookLink');
 forFacebook.addEventListener('click', () =>{
     window.location.href="https://www.facebook.com/misternetfitnessclub"
 })
+
+//CLOCK
+
+setInterval(setClock, 1000);
+
+function setClock() {
+  const hourHand = document.querySelector('[data-hour-hand]');
+  const minuteHand = document.querySelector('[data-minute-hand]');
+  const secondHand = document.querySelector('[data-second-hand]');
+
+  const currentDate = new Date();
+  const secondRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+  setRotation(secondHand, secondRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock();
