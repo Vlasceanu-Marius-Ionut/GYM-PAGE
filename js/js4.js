0270172dac3bde98ae252a1b6c03e05f1167349a
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-09-11 23:25:07 +0300

    clock

--- js/js4.js
+++ js/js4.js
@@ -41,26 +41,24 @@ forFacebook.addEventListener('click', () =>{
 })
 
 //CLOCK
+setInterval(setClock, 1000)
 
-setInterval(setClock, 1000);
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
 
 function setClock() {
-  const hourHand = document.querySelector('[data-hour-hand]');
-  const minuteHand = document.querySelector('[data-minute-hand]');
-  const secondHand = document.querySelector('[data-second-hand]');
-
-  const currentDate = new Date();
-  const secondRatio = currentDate.getSeconds() / 60;
-  const minutesRatio = (secondRatio + currentDate.getMinutes()) / 60;
-  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
-
-  setRotation(secondHand, secondRatio);
-  setRotation(minuteHand, minutesRatio);
-  setRotation(hourHand, hoursRatio);
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
 }
 
 function setRotation(element, rotationRatio) {
-  element.style.setProperty('--rotation', rotationRatio * 360);
+  element.style.setProperty('--rotation', rotationRatio * 360)
 }
 
-setClock();
+setClock()
